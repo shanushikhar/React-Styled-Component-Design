bc3bd0146669db42bf39c1791b83aa1628aef0bd
Shanu Shikhar <thisisshanushikhar@gmail.com>
2023-03-28 20:37:11 +0530

    Added Login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import ProductList from "./pages/ProductList";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
-  return <Register/>
+  return <Login />
 };
 
 export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,94 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    background:linear-gradient(rgba(0,0,0,0.5),rgba(255,255,255,0.6)), url("https://msingermany.co.in/wp-content/uploads/2019/12/%E2%80%94Pngtree%E2%80%94flat-wind-men-s-business_4575829-400x400.png") center;
+    background-repeat: no-repeat;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const Wrapper = styled.div`
+    /* padding: 80px; */
+    width: 23%;
+`
+
+const Title = styled.h1`
+    font-size: 28px;
+    font-weight: 600;
+`
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`
+const Input = styled.input`
+    flex: 1;
+    /* min-width: 30%; */
+    margin: 5px 0;
+    margin-right: 7px;
+    padding: 9px;
+    outline: none;
+
+    &:focus{
+        border: 2px solid teal;
+    }
+`
+
+const Button = styled.button`
+    padding: 10px 20px;
+    width: 30%;
+    cursor: pointer;
+    font-weight: bold;
+    border: none;
+    background: teal;
+    &:hover{
+        color: #fff;
+        background-color: #548585;
+    }
+    margin: 8px 0;
+`
+const LinkContainer = styled.div`
+    background-color: #fff;
+    padding: 10px;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+`
+const Link = styled.a`
+    color: black;
+    text-decoration: underline;
+    margin-top: 5px;
+    cursor: pointer;
+    font-size: 18px;
+    &:hover{
+        color: #fff;
+        background-color: #548585;
+    }
+`
+
+const Register = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Title>SIGN IN</Title>
+            <Form>
+                <Input placeholder="username"/>
+                <Input placeholder="password"/>
+                <Button>LOGIN</Button>
+                <LinkContainer>
+                    <Link>
+                        Do not remember the password?
+                    </Link>
+                    <Link>
+                        Create an account
+                    </Link>
+                </LinkContainer>
+            </Form>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Register
